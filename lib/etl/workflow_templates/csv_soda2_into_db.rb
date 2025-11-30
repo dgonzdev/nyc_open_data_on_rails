723af2aba6bdf387f60733521ba5a34fd5e40e1e
dgonzdev <dgonzdev837@gmail.com>
2025-11-30 17:56:44 -0500

    rename workflow template

--- lib/etl/workflow_templates/csv_soda2_into_db.rb
+++ lib/etl/workflow_templates/csv_soda2_into_db.rb
@@ -1,6 +1,6 @@
 module Etl
   module WorkflowTemplates
-    module CsvSoda2IntoPrimaryDbWorkflowTemplate
+    module CsvSoda2IntoDb
       module_function
 
       # https://github.com/thbar/kiba/wiki/How-to-define-ETL-jobs-with-Kiba
