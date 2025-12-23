0e8593cab7c548741f8dff06cc61ecfaf8a8bc2f
dgonzdev <dgonzdev837@gmail.com>
2025-12-22 19:30:25 -0800

    add api endpoints

--- app/models/office_of_citywide_event_coordination_and_management/nyc_permitted_event_information_historical.rb
+++ app/models/office_of_citywide_event_coordination_and_management/nyc_permitted_event_information_historical.rb
@@ -1,5 +1,11 @@
 module OfficeOfCitywideEventCoordinationAndManagement
   class NycPermittedEventInformationHistorical < ApplicationRecord
     self.table_name = :nyc_permitted_event_information_historicals
+
+    SODA2_API_ENDPOINT = "https://data.cityofnewyork.us/resource/bkfu-528j.json"
+    SODA2_CSV_API_ENDPOINT = "https://data.cityofnewyork.us/resource/bkfu-528j.csv"
+
+    SODA3_API_ENDPOINT = "https://data.cityofnewyork.us/api/v3/views/bkfu-528j/query.json"
+    SODA3_CSV_API_ENDPOINT = "https://data.cityofnewyork.us/api/v3/views/bkfu-528j/query.csv"
   end
 end
