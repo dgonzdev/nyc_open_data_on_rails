40f0fab1940227011279771e6bbb975e7e675c89
dgonzdev <dgonzdev837@gmail.com>
2025-12-22 19:25:54 -0800

    add a model

---
+++ app/models/office_of_citywide_event_coordination_and_management/nyc_permitted_event_information_historical.rb
@@ -0,0 +1,5 @@
+module OfficeOfCitywideEventCoordinationAndManagement
+  class NycPermittedEventInformationHistorical < ApplicationRecord
+    self.table_name = :nyc_permitted_event_information_historicals
+  end
+end
